925adf15287eb73a7cfed991614108049be67ce4
MrTw1ce <lucas_martins_2024@hotmail.com>
2024-10-30 19:20:59 +0000

    Add files via upload

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SQLModel.App1.Models;
+
+namespace SQLModel.NETCoreWebAPI.ApplicationDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SQLModel.App1.Models{
+
+    public class Product{
+        [Key]
+        public string Name {get;set;}
+        public string Image {get;set;}
+        public string Category {get;set;}
+        public float Price {get;set;}
+
+    }
+
+    public class User{
+        [Key]
+        public int Id {get;set;}
+        public string Email {get;set;}
+        public string Password {get;set;}
+        public string Role {get;set;}
+    }
+    
+    public class Order{
+        [Key]
+        public int Id {get;set;}
+        public string UserEmail {get;set;}
+        public string OrderJson {get;set;}
+    }
+}
